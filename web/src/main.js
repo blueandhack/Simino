133c81cbb15697903050e944c5c9b540efe13ad7
Yoga Lin <blueandhack@gmail.com>
2017-10-02 12:38:15 -0700

    add github button

--- web/src/main.js
+++ web/src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import ElementUI from 'element-ui'
+import VueGitHubButtons from 'vue-github-buttons'
 import App from './App'
 import router from './router'
 
 import 'element-ui/lib/theme-default/index.css'
+import 'vue-github-buttons/dist/vue-github-buttons.css'
 
 Vue.use(VueResource)
 Vue.use(ElementUI)
+Vue.use(VueGitHubButtons)
 
 Vue.config.productionTip = false
 
@@ -18,5 +21,5 @@ new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: {App}
 })
