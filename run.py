f58e63ae2f59eec83bc6c599cb656405ca592304
Yoga Lin <blueandhack@gmail.com>
2017-10-02 04:00:34 -0700

    update run.py

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import requests
 import json
 
 usbport = '/dev/cu.usbmodem1441'
+token = ''
+url = ''
 
 def main():
     ser = serial.Serial(usbport, 9600)
@@ -11,6 +13,9 @@ def main():
         score = score.decode("utf-8")
         score = int(score)
         print('score ', score)
+        data = {'token':token, 'score':score }
+        data = json.dump(data)
+        requests.post(url, data = data)
 
 if __name__ == '__main__':
     main()
